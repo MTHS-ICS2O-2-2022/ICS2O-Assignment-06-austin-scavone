c797502e22b1d3e8e5adc71cd3ea086dd82266e3
austin <124389297+austin-scavone@users.noreply.github.com>
2023-06-20 00:12:22 +0000

    fixes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) austin scavone All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: austin scavone
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+window.onload = getduck = async () => {
+  try {
+    // Get duck image
+    const badStatusCodes = [duckImage]
+    let randomInt = Math.floor(Math.random() * badStatusCodes.length)
+    let randomStatusCode = badStatusCodes[randomInt]
+
+    const duckImage = "https://random-d.uk/api" + randomStatusCode
+    console.log(randomInt)
+    // ouptut duck image
+    document.getElementById("duck-image").innerHTML =
+      "<img id='duck' src=" + duckImage + "></img>"
+  } catch (err) {
+    console.error(err)
+  }
+}
+getduck()
